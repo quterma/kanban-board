4353345beb70990a88fcd24c4914480445f5079c
Quterma <wolf921@mail.ru>
2020-10-29 13:58:55 +0300

    add hash

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "@atlaskit/css-reset";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Provider store={store}>
 				<App />
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
